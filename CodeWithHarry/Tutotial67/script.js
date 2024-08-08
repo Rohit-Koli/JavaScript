3325ad4458de0d2ee4050a9de7da9ffe998a1bc2
Rohit Koli <kolirohit1252@gmail.com>
2024-08-08 08:16:00 +0530

    Added new Folder for practicing DOM object's

---
+++ CodeWithHarry/Tutotial67/script.js
@@ -0,0 +1,11 @@
+console.log("Hello")
+let childNo=document.body.childNodes[1]
+console
+childNo.childNodes[4]
+childNo.lastChild
+
+childNo.lastElementChild
+childNo.firstElementChild
+childNo.childElementCount
+childNo.lastElementChild.parentNode
+childNo.lastElementChild.parentNode.style.backgroundColor="Red";
