8236fcec5bf8d238f9c8407e7194f3014bdbb92a
Rohit Koli <kolirohit1252@gmail.com>
2024-08-12 23:15:51 +0530

    did some Work on MAP in JS

--- IMP/Map/index.js
+++ IMP/Map/index.js
@@ -14,8 +14,45 @@ let myMap= new Map([
     [true,'Yes']
 ]);
 
+//Accesing Elements of MAP's 
 console.log(myMap.get('name')) //Rohit
 
 console.log(myMap.get('gender')) //Male
 
 console.log(myMap.get('age')) //22
+
+//Inserting ELement and key in MAP
+
+myMap.set('Occupation','SDE');
+
+console.log(myMap.get('Occupation')) //SDE
+
+//Checking for keys
+
+console.log(myMap.has('Occupation')) //true
+
+//Deletion in MAP
+
+myMap.delete('Occupation')
+
+console.log(myMap)
+/*
+Map(4) {
+  'name' => 'Rohit',
+  'age' => 22,
+  'gender' => 'Male',
+  true => 'Yes'
+}
+*/
+
+//Remove all key value pairs from map's
+let mp = new Map([
+    ['aknk','jnbkd'],
+    ['frgv','fref'],
+    [true,false]
+])
+
+console.log(mp) //Map(3) { 'aknk' => 'jnbkd', 'frgv' => 'fref', true => false }
+
+mp.clear()
+console.log(mp) //Map(0) {}
